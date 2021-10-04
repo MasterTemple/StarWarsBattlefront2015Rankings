2d888d04745baac0c77d6f72384f403825617e72
MasterTemple <dgmastertemple@protonmail.com>
2021-10-04 10:52:16 -0700

    refresh token

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ client.once('ready', async () => {
     // await update_discord_roles(client, config)
 
     // await create_leaderboard(client, config)
-    // await auto_refresh_token(config)
+    await auto_refresh_token(config)
 
     console.log(`${config.bot_name} v${parseFloat(config.version).toFixed( 1)} is fully operational.`) //logs that the bot is ready
 
